13814ab0b913896fa3b6854579b57bdb93e1377a
lizhao <2560670157@qq.com>
2026-07-31 21:21:18 +0800

    chore: components.d.ts 自动登记 BookCard

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -47,6 +47,7 @@ declare module 'vue' {
     ElLink: typeof import('element-plus/es')['ElLink']
     ElMain: typeof import('element-plus/es')['ElMain']
     ElOption: typeof import('element-plus/es')['ElOption']
+    ElOptionGroup: typeof import('element-plus/es')['ElOptionGroup']
     ElPagination: typeof import('element-plus/es')['ElPagination']
     ElPopover: typeof import('element-plus/es')['ElPopover']
     ElRadio: typeof import('element-plus/es')['ElRadio']
